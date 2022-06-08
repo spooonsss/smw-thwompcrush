151b3210b424c1e381f634cf75e69f9cfd56b2cb
spooonsss <spooonsss123@gmail.com>
2022-06-08 09:41:44 -0500

    Fix !1588, fix layer 2, fix vertical level

--- thwompcrush.asm
+++ thwompcrush.asm
@@ -1,13 +1,10 @@
 ;This block shatters when hit by a Thwomp
-;by Roy
+;by Roy, modified by spooonsss
 ;act like 130
 
 print "Thwomp Crush Block\nA block that shatters when hit by a Thwomp."
 
-!RiseUp = 1 ; rise up (0 = Set it to 0 if you don't want the Thwomp to rise up)
-
 db $37
-
 JMP Return : JMP Return : JMP Return
 JMP SpriteV : JMP Return
 JMP Return : JMP Return
@@ -17,15 +14,9 @@ JMP Return : JMP Return
 
 !CUSTOM_SPRITE = $26    ; set custom sprite
 !NORMAL_SPRITE = $26    ; set normal sprite
-
-!SHAKE_TIMER = $18      ; time to shake the ground
-!SOUND_EFFECT = $09     ; set sound effect
-!TIME_TO_RISE = $40     ; time to rise up
-!SHATTER_TYPE = $00     ; 00 = normal, FF = flashing
-
+!THWOMP_GOES_THROUGH = 0 ; 1 => thwomp crushes the block and goes straight through (this block acts as 025)
 
 SpriteV:
-	LDX $15E9|!addr         ; load sprite index
 	LDA !7FAB10,x           ; \ check if its a custom sprite...
 	AND #$08                ;  |
 	BEQ Normal              ; /
@@ -34,7 +25,7 @@ Custom:
 	CMP #!CUSTOM_SPRITE     ;  |
 	BNE Return              ; /
 	BRA Status
-	
+
 Normal:
 	LDA !9E,x               ; \ if the sprite is a thwomp...
 	CMP #!NORMAL_SPRITE     ;  |
@@ -45,50 +36,43 @@ Status:
 	BNE Return              ; /
 
 Crush:
-	LDA !E4,x               ; \ setup block properties
-	STA $9A                 ;  |
-	LDA !14E0,x             ;  |
-	STA $9B                 ;  |
-	LDA !D8,x               ;  |
-	CLC                     ;  |
-	ADC #$20                ;  | set y position
-	STA $98                 ;  |
-	LDA !14D4,x             ;  |
-	ADC #$00                ;  |
-	STA $99                 ; /
-	
-	JSR Shatter_Routine     ; go to shatter block routine
-	
-	LDA $9A                 ; \ setup block properties
-	CLC                     ;  |
-	ADC #$10                ;  | set x position
-	STA $9A                 ;  |
-	LDA $9B                 ;  |
-	ADC #$00                ;  |
-	STA $9B                 ; /
-	
-	JSR Shatter_Routine     ; go to shatter block routine
+	LDA $0F ; $0F must be preserved https://smwc.me/m/smw/rom/019138/ (shatter_block/ChangeMap16/$00BEB0 clobbers it)
+	PHA
+	%sprite_block_position()
 
-	if !RiseUp
-		LDA #!SHAKE_TIMER       ; \ amount of time the ground shakes
-		STA $1887|!addr         ; / 
-		LDA #!SOUND_EFFECT      ; \ set sound effect
-		STA $1DFC|!addr         ; / sound effect bank $1DFC
-		INC !C2,x               ; set Thwomp to rise up
-		LDA #!TIME_TO_RISE      ; \ amount of time it takes Thwomp to get back up
-		STA !1540,x             ; /
+	; preserve $98/$9A for vertical levels
+	REP #$20
+	LDA $98
+	PHA
+	LDA $9A
+	PHA
+	SEP #$30
+	; shatter_block doesn't support this
+	; LDY.b #!SHATTER_TYPE      ; set animation for the shatter
+	%shatter_block()
+	REP #$21
+	PLA
+	STA $9A
+	PLA
+	STA $98
+
+	; offset to the block to the right
+	LDA $9A
+	; CLC
+	ADC #$0010
+	STA $9A
+	SEP #$30
+
+	%shatter_block()
+
+	if !THWOMP_GOES_THROUGH
+		LDA #$25 : STA $1693|!addr
+		LDY #$00
+	else
+		LDA #$30 : STA $1693|!addr
+		LDY #$01
 	endif
-
+	PLA
+	STA $0F
 Return:
-	RTL                     ; return
-
-Shatter_Routine:
-	%erase_block()
-	PHK                     ; push bank register
-	PER Return_Add-$01	    ; push return address -1 
-	PEA $8020		        ; push return address containing PLB + RTL
-	LDY #!SHATTER_TYPE      ; set animation for the shatter
-	JML $0199F8|!bank       ; shatter block routine
-Return_Add:
-	RTS                     ; return
-
+	RTL
